62546117ba239fd8ec801ec7bc7a1f9f7e23a20c
ZakariaMarzouq <76443610+ZakariaMarzouq@users.noreply.github.com>
2021-08-10 14:42:24 +0200

    Creation controllers user

--- controllers/user.js
+++ controllers/user.js
@@ -1,14 +1,13 @@
 const bcrypt = require("bcrypt");
 const maskdata = require("maskdata");
 const jwt = require("jsonwebtoken");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
 dotenv.config();
 
 const User = require("../models").User;
 
 //inscription :
-
 exports.signup = (req, res) => {
   bcrypt
     .hash(req.body.password, 10)
@@ -33,32 +32,30 @@ exports.signup = (req, res) => {
 };
 
 //Connexion avec un compte existant en BDD :
-
 exports.login = (req, res) => {
   User.findOne({ where: { email: maskdata.maskEmail2(req.body.email) } })
-      .then(user => {
-          // Si on ne trouve aucun utilisateur
-          if (!user) {
-              return res.status(401).json({ error: 'Utilisateur non trouvé !' })
+    .then((user) => {
+      // Si on ne trouve aucun utilisateur
+      if (!user) {
+        return res.status(401).json({ error: "Utilisateur non trouvé !" });
+      }
+      // Bcrypt compare le mot de passe avec le hash enregistré
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then((valid) => {
+          // Si les résultats sont différents
+          if (!valid) {
+            return res.status(401).json({ error: "Mot de passe incorrect !" });
           }
-          // Bcrypt compare le mot de passe avec le hash enregistré
-          bcrypt.compare(req.body.password, user.password)
-              .then(valid => {
-                  // Si les résultats sont différents
-                  if (!valid) {
-                      return res.status(401).json({ error: 'Mot de passe incorrect !' })
-                  }
-                  res.status(200).json({
-                      userId: user.id,
-                      // Génère un token grâce au package jsonwebtoken
-                      token: jwt.sign(
-                          { userId: user.id },
-                          process.env.TOKEN,
-                          { expiresIn: '24h' }
-                      )
-                  });
-              })
-              .catch(error => res.status(500).json({ error }));
-      })
-      .catch(error => res.status(500).json({ error }));
+          res.status(200).json({
+            userId: user.id,
+            // Génère un token grâce au package jsonwebtoken
+            token: jwt.sign({ userId: user.id }, process.env.TOKEN, {
+              expiresIn: "24h",
+            }),
+          });
+        })
+        .catch((error) => res.status(500).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
 };
